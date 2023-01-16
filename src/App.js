f6aef26c9d616fb1b5adeb041ce65ae640d2c27c
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-16 10:39:17 -0300

    finaliza o projeto

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { ButtonTema } from "./Components/UI";
+import SwitcherTema from "./Components/SwitcherTema";
 
 function App() {
+
+  const [tema, setTema] = useState(true)
+
+  const toggleTema = () => {
+    setTema(( tema ) => !tema)
+  }
+
   return (
     <>
-    <GlobalStyle />
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
+
+      <GlobalStyle />
+
+      <ButtonTema onClick={toggleTema}>
+        <SwitcherTema tema={tema}/>
+      </ButtonTema>
+
       <Cabecalho />
       <Container />
+    </ThemeProvider>
     </>
   );
 }
